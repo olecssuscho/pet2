368f0b6ddff7170efe164f7bb42b276489df7987
olecs <olecs@SANYA>
2026-05-28 14:59:51 +0300

    dbmodels

--- Schemas/dbmodels.py
+++ Schemas/dbmodels.py
@@ -1,5 +1,5 @@
-from sqlalchemy import Column,String,Boolean,Float,Integer
-from sqlalchemy.orm import declarative_base 
+from sqlalchemy import Column,String,Boolean,Float,Integer,Enum,ForeignKey
+from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
 
@@ -14,3 +14,34 @@ class User(Base):
     balance  = Column(Float)
     is_blocked  = Column(Boolean)
     created_at = Column(String)
+
+class Transaction(Base):
+
+    __tablename__ = "Transaction"
+
+    id = Column(Integer,primary_key=True,autoincrement=True)
+    sender_id = Column(Integer,ForeignKey(User.id))
+    reciever_id = Column(Integer,ForeignKey(User.id))
+    amount = Column(Float)
+    status = Column(String,Enum("pending","success","failed","frozen",name = "status"),default="pending")
+    type = Column(String,Enum("transfer","deposit","refund",name = "type"))
+
+class PaymentRequest(Base):
+
+    __tablename__ = "PaymentRequest"
+
+    id = Column(Integer, primary_key=True,autoincrement=True)
+    from_user_id = Column(Integer,ForeignKey(User.id))
+    to_user_id = Column(Integer,ForeignKey(User.id))
+    amount = Column(Float)
+    message = Column(String)
+    status = Column(String,Enum("pending","success","failed","frozen",name = "status"),default="pending")
+
+class Refund(Base):
+
+    __tablename__ = "Refund"
+
+    id = Column(Integer, primary_key=True,autoincrement=True)    
+    transaction_id = Column(Integer,ForeignKey(Transaction.id))
+    reason = Column(String)
+    status = Column(String,Enum("pending","success","failed","frozen",name = "status"),default="pending")
--- Schemas/models.py
+++ Schemas/models.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from typing import Literal
 
 class User(BaseModel):
     email:str
@@ -7,3 +8,22 @@ class User(BaseModel):
     balance:float
     is_blocked:bool
     created_at:str
+
+class Transaction(BaseModel):
+    sender_id:int
+    reciever_id:int
+    amount:float
+    status:Literal["pending","success","failed","frozen"]="pending"
+    type:Literal["transfer","deposit","refund"]
+
+class PaymentRequest(BaseModel):
+    from_user_id:int
+    to_user_id:int
+    amount:float
+    message:str
+    status:Literal["pending","success","failed","frozen"]="pending"
+
+class Refund(BaseModel):
+    transaction_id:int
+    reason:str
+    status:Literal["pending","success","failed","frozen"]="pending"
