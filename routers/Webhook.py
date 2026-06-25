b7360227bccfe5a1e39e4f7745a92a749a5ea907
olecs <olecs@SANYA>
2026-06-25 15:21:39 +0300

    add get and delete request for webhook

--- routers/Webhook.py
+++ routers/Webhook.py
@@ -2,10 +2,18 @@ from fastapi import APIRouter,Depends
 from schemas.models import WebhookMODEL,UserMODEL
 from sqlalchemy.orm import Session
 from dependency import get_current_user,get_db
-from services.Webhook import webhook_post_service
+from services.Webhook import webhook_post_service,webhook_get_service,webhook_delete_service
 
 router = APIRouter()
 
 @router.post("/webhook/register")
 def webhook_post(url:str = None,user:UserMODEL = Depends(get_current_user),db:Session = Depends(get_db)):
     return webhook_post_service(url,user,db)
+
+@router.get("/webhook")
+def webhook_get(user:UserMODEL = Depends(get_current_user),db:Session = Depends(get_db)):
+    return webhook_get_service(user,db)
+
+@router.delete("/webhook/{id}")
+def webhook_delete(id:int,user:UserMODEL = Depends(get_current_user),db:Session = Depends(get_db)):
+    return webhook_delete_service(id,user,db)
--- services/Webhook.py
+++ services/Webhook.py
@@ -11,3 +11,11 @@ def webhook_post_service(url:str,user:UserDB,db:Session):
     db.add(Webhook)
     db.commit()
     return "Success"
+
+def webhook_get_service(user:UserDB,db:Session):
+    return db.query(WebhookDB).filter(WebhookDB.user_id == user.id).all()
+
+def webhook_delete_service(id:int,user:UserDB,db:Session):
+    db.delete(db.query(WebhookDB).filter(WebhookDB.id == id).first())
+    db.commit()
+    return "Success"
